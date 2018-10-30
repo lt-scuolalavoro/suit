fc8a6db4d71a95b23dc048a421a1c21b9d4ef81a
Giuseppe Lepore <giuseppejoelepore@gmail.com>
2018-10-30 18:00:12 +0100

    Format code

--- candidates.c
+++ candidates.c
@@ -11,46 +11,45 @@ struct candidate {
   int age;
   float salary;
 };
-  //declaration function
+//declaration function
 
-  //this function, given in input a FILE, returns its number of lines;
+//this function, given in input a FILE, returns its number of lines;
 int findLines(FILE *);
-  //this function, given in input lastName, the struct of the candidates and the numbers of the candidates, returns the age of the person;
-char age(char [],struct candidate *dip,int numDip);
+//this function, given in input lastName, the struct of the candidates and the numbers of the candidates, returns the age of the person;
+char age(char [], struct candidate *dip, int numDip);
 
 
 void main(){
   //Database file must be in the same directory level;
   FILE *fp;
-  fp = fopen("database.txt","r");
+  fp = fopen("database.txt", "r");
   int numDip = findLines(fp);
 
   int i;
-
   //declaration that defines a physically grouped list of variables;
   struct candidate dip[numDip];
   //rewind function sets the file position to the beginning of the file of the given stream;
   rewind(fp);
 
-  for(i=0;i<numDip;i++){
+  for(i=0; i<numDip; i++){
     //the function fscanf reads formatted input from a stream;
-    fscanf(fp,"%i %s %s %i %f",&dip[i].id, dip[i].firstName, dip[i].lastName, &dip[i].age, &dip[i].salary);
+    fscanf(fp, "%i %s %s %i %f", &dip[i].id, dip[i].firstName, dip[i].lastName, &dip[i].age, &dip[i].salary);
   }
-  printf("%i\n",dip[6].age);
+  printf("%i\n", dip[6].age);
 
   char appoggio[50];
   printf("lastName: ");
-  scanf("%s",appoggio);
-  printf("Age : %i\n",age(appoggio,dip,numDip));
+  scanf("%s", appoggio);
+  printf("Age : %i\n", age(appoggio,dip,numDip));
 
   fclose(fp);
 }
 
-char age(char appoggio[],struct candidate *dip, int numDip){
+char age(char appoggio[], struct candidate *dip, int numDip){
   int trovato = 0;
-  for(int i=0;i<numDip;i++){
+  for(int i=0; i<numDip; i++){
     //the function strcmp compares the string pointed to, by str1 to the string pointed to by str2;
-    if(strcmp(appoggio,dip[i].lastName)==0){
+    if(strcmp(appoggio, dip[i].lastName)==0){
       trovato = 1;
       return dip[i].age;
     }
@@ -63,7 +62,7 @@ int findLines(FILE *fp){
   int num=0;
   //while the lines is not NULL increase the counter variable;
   while(fgets(ch, sizeof(ch), fp) != NULL){
-        lines++;
-    }
+    lines++;
+  }
   return lines;
 }
