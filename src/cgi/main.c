3045fd797b0004a7cf3bbc61de745b42126eaf92
Raffaele <raffydelbasso@gmail.com>
2019-01-25 08:17:06 -0800

    Close #48 - Create button3 backend

--- src/cgi/main.c
+++ src/cgi/main.c
@@ -13,10 +13,15 @@ int main(int argc, char const *argv[])
     printf("</head>\n");
     printf("<body>\n");
     
-    if(!strcmp(action, "btn=2"))
-        // Call the function that executes the query in the db and prints the result in a table
+    if(!strcmp(action, "btn=2")) {
+    	// Call the function that executes the query in the db and prints the result in a table
         printCandidates("WHERE removed = 0");
-    
+    } else {
+    	if(!strcmp(action, "btn=3")) {
+    		printCandidates("WHERE removed = 1");
+    	}
+    }
+
     printf("<br>");
     printf("<a href=\"../home.html\">Back</a>\n");
     printf("</body>\n");
