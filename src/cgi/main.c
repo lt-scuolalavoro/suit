f90ac01d5f6ccfe7b8bd858c936f4c1e65661b25
unknown <marcytutorial@gmail.com>
2019-01-25 19:13:57 +0100

    Close #49 - Improve graphics

--- src/cgi/main.c
+++ src/cgi/main.c
@@ -7,10 +7,12 @@ int main(int argc, char const *argv[])
     printOpeningTags();
     
     if(!strcmp(action, "btn=2")) {
+    	printf("<h1>Suit Database - Candidati non rimossi</h1>");
     	// Call the function that executes the query in the db and prints the result in a table
         printCandidates("WHERE removed = 0");
     } else {
     	if(!strcmp(action, "btn=3")) {
+    		printf("<h1>Suit Database - Candidati rimossi</h1>");
     		printCandidates("WHERE removed = 1");
     	}
     }
