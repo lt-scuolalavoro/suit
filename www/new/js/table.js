24bcf4e98b2dc2778bb4334df9ba0d1edb76b4dd
Raffaele <raffydelbasso@gmail.com>
2019-02-21 20:00:26 +0100

    Create filters

--- www/new/js/table.js
+++ www/new/js/table.js
@@ -2,10 +2,15 @@ Vue.component("suit-table", {
   data() {
     return {
       status: null,
-      filter: null,
+      filter: '',
       sortBy: "name",
       items: [],
       filteredItems: [],
+      condition:"",
+      range1:0,
+      range2:0,
+      options: [{text: 'Show unemployed candidates', value: 'unemployed'}, 
+      {text: 'Show deleted candidates', value: 'deleted'}, {text: 'Apply salary range', value: 'range'}],
       fields: [
         { key: "firstName", sortable: true, label: "First name" },
         { key: "lastName", sortable: true, label: "Last name" },
@@ -17,6 +22,21 @@ Vue.component("suit-table", {
     };
   },
   methods: {
+  	applyFilter(checked) {
+  		this.condition="";
+  		if(checked.includes("unemployed")) {
+  			this.condition+="u";
+  		}
+  		if(checked.includes("deleted")) {
+  			this.condition+="d";
+  		}
+      if(checked.includes("range")) {
+        range1=parseInt(document.getElementById("range-1").value);
+        range2=parseInt(document.getElementById("range-2").value);
+        this.condition+="r";
+      }
+  		this.typeSearch();
+  	},
     showModalInfo(button) {
       this.modalInfo.content = "Hello";
       this.$root.$emit("bv::show::modal", "modalInfo", button);
@@ -42,6 +62,7 @@ Vue.component("suit-table", {
       }
     },
     typeSearch(){
+<<<<<<< HEAD
       let filteredList=[]
       let i
       n = this.items.length
@@ -54,6 +75,54 @@ Vue.component("suit-table", {
       this.filteredItems=filteredList
     },
     
+=======
+    	let filteredList=[]
+    	let i
+    	for(i=0; i<this.items.length; i++){
+    		if(this.filter === '') {
+    				if (this.condition==="u" && this.items[i].employed==0 && this.items[i].removed==0) {
+    					filteredList.push(this.items[i]);
+              			continue;
+    				}
+    				if (this.condition==="d" && this.items[i].employed==1 && this.items[i].removed==1) {
+    					filteredList.push(this.items[i]);
+              			continue;
+    				}
+    				if (this.condition==="ud" && this.items[i].employed==0 && this.items[i].removed==1) {
+    					filteredList.push(this.items[i]);
+              			continue;
+   					}
+		            if (this.condition==="r" && this.items[i].salary>=range1 && this.items[i].salary<=range2) {
+		              filteredList.push(this.items[i]);
+		              continue;
+		            }
+    		} else {
+    			if(this.items[i].lastName.toLowerCase().includes(this.filter.toLowerCase()) || 
+    			this.items[i].firstName.toLowerCase().includes(this.filter.toLowerCase())){
+            if (this.condition!='') {
+              if (this.condition==="u" && this.items[i].employed==0 && this.items[i].removed==0) {
+                filteredList.push(this.items[i]);
+                continue;
+              }
+        
+              if (this.condition==="d" && this.items[i].employed==1 && this.items[i].removed==1) {
+                filteredList.push(this.items[i]);
+                continue;
+              }
+        
+              if (this.condition==="ud" && this.items[i].employed==0 && this.items[i].removed==1) {
+                filteredList.push(this.items[i]);
+                continue;
+              }
+            } else {
+              filteredList.push(this.items[i]);
+            }
+    			}
+    		}
+    	}
+    	this.filteredItems=filteredList;
+    }
+>>>>>>> Create filters
   },
   mounted() {
     axios.get("cgi/users.cgi")
@@ -73,28 +142,62 @@ Vue.component("suit-table", {
         </b-input-group-append>
         </b-input-group>
     </b-form-group>
-    <b-form-checkbox id="checkbox1"
-                     v-model="status"
-                     value="accepted"
-                     unchecked-value="not_accepted" class="mt-2 ml-3" >
-    Show deleted candidates
-    </b-form-checkbox>
+    <div style="padding-left: 1%; padding-top: 3px;">
+    <b-button v-b-toggle.collapse1 title="Show filters" size="sm"><i class="fas fa-cog"></i></b-button>
+    </div>
+    <div style="padding-left: 1%; padding-bottom: 2%; ">
+    <b-collapse id="collapse1" class="mt-2">
+    <div style="padding-left: -1%; padding-bottom: -2%; padding-top: -3%;">
+    <b-card>
+    <div>
+    <b-form-checkbox-group plain stacked :options="options" @change="applyFilter">
+    <div>
+    Min: <b-form-input type="number" id="range-1" min="0" size="sm" max="10000"></b-form-input>
+    Max: <b-form-input type="number" id="range-2" min="0" size="sm" max="10000"></b-form-input>
+    </div>  
+    </b-form-checkbox-group>
+    </div>
+    </b-card>
+    </div>
+  </b-collapse>
+  </div>
+   
 </b-row>
-
      <b-table :sort-by.sync="sortBy"
             striped
             hover
-             :items="filter == null ? items : filteredItems"
+             :items="filter=='' && condition=='' ? items : filteredItems"
              :fields="fields">
+
      <template slot="action" slot-scope="row">
          
          <b-button-group>
-            <b-button @click.stop="showModalInfo(this)" title="Show details" variant="outline-primary" size="sm"><i class="fas fa-ellipsis-v"></i></b-button>
-            <b-button variant="outline-primary" onclick="location.href='user.html'" title="Edit candidate" size="sm"><i class="fas fa-pencil-alt"></i></b-button>
+            <b-button variant="outline-primary" size="sm" @click.stop="row.toggleDetails"><i class="fas fa-ellipsis-v"></i></b-button>
+            <b-button variant="outline-primary" title="Edit candidate" size="sm"><i class="fas fa-pencil-alt"></i></b-button>
             <b-button @click.stop="showModalDelete()" variant="outline-danger" title="Delete candidate"size="sm"><i class="far fa-trash-alt"></i></b-button>
          </b-button-group>
 
      </template>
+     <template slot="row-details" slot-scope="row">
+      <b-card>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Full name: </b></b-col>
+          <b-col>{{ row.item.firstName +" "+ row.item.lastName }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Birth date: </b></b-col>
+          <b-col>{{ row.item.birthDate }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Employed: </b></b-col>
+          <b-col>{{ row.item.Employed == 0 ? 'Yes' : 'No' }}</b-col>
+        </b-row>
+        <b-row class="mb-2">
+          <b-col sm="3" class="text-sm-right"><b>Salary: </b></b-col>
+          <b-col>{{"€ "+row.item.salary }}</b-col>
+          </b-row>
+      </b-card>
+    </template>
     </b-table>
 
     <b-modal id="modalInfo" @hide="resetModal" :title="modalInfo.title" ok-only>
