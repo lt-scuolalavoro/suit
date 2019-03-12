0f38d4cd2118b8cd00baaac4e571223db4d8db94
cosmo4u <martinopagano_01@libero.it>
2019-03-12 10:00:56 +0100

    Implement person object in personForm.js
    Co-authored-by: Simone235 <simone.cacciapaglia@gmail.com>
    Co-authored-by: RobertoClemente25 <roby.heimeman@gmail.com>

--- www/new/js/personForm.js
+++ www/new/js/personForm.js
@@ -1,48 +1,51 @@
 Vue.component("person-form", {
     data(){
         return {
-            firstName: '',
-            lastName: '',
-            birthDate: '',
-            employed: '',
-            salary: '',
-            notes: '',
-            contacts: [],
-            contactName: '',
-            contactLink: '',
-            fields: [
-                {key: "name", label: "Name"},
-                {key: "link", label: "Link"},
-                {key: "action", label: "Action"}
-            ],
-            date: 'date'
+            person: {
+                firstName: '',
+                lastName: '',
+                birthDate: '',
+                employed: '',
+                salary: '',
+                notes: '',
+                contacts: [],
+                contactName: '',
+                contactLink: ''
+            },
+
+             fields: [
+                    {key: "name", label: "Name"},
+                    {key: "link", label: "Link"},
+                    {key: "action", label: "Action"}
+                ],
+                date: 'date'
         };
     },
 
     methods: {
         setFirstName(value) {
-            this.firstName = value;
+            this.person.firstName = value;
         },
         setLastName(value) {
-            this.lastName = value;
+            this.person.lastName = value;
         },
         setBirthDate(value) {
-            this.birthDate = value;
+            this.person.person.birthDate = value;
         },
         setEmployed(value) {
-            this.employed = value;
+            this.person.employed = value;
         },
         setSalary(value) {
-            this.salary = value;
+            this.person.salary = value;
         },
         setNotes(value) {
-            this.notes = value;
+            this.person.notes = value;
         },
         setContact(){
-            if(this.contactName != '' && this.contactLink != ''){
-                this.contacts.push({name: this.contactName, link: this.contactLink});
-                this.contactName = '';
-                this.contactLink = '';
+            if(this.person.contactName != '' && this.person.contactLink != ''){
+                this.person.contacts.push({name: this.person.contactName, link: this.person.contactLink});
+                this.person.contactName = '';
+                this.person.contactLink = '';
             }
         },
         showModalDelete(button){
@@ -58,8 +61,7 @@ Vue.component("person-form", {
                       label-for="inputFirstName">
             <b-form-input id="inputFirstName"
                           placeholder="insert first name here"
-                          v-model="firstName"
-                          @input="setFirstName(firstName)">
+                          v-model="person.firstName">
             </b-form-input>
         </b-form-group>
 
@@ -69,8 +71,7 @@ Vue.component("person-form", {
                       label-for="inputLastName">
             <b-form-input id="inputLastName"
                           placeholder="insert last name here"
-                          v-model="lastName"
-                          @input="setLastName(lastName)">
+                          v-model="person.lastName">
             </b-form-input>
         </b-form-group>
 
@@ -81,8 +82,7 @@ Vue.component("person-form", {
             <b-form-input id="inputBirthDate"
                           :type = "date"
                           placeholder="insert the birth date here "
-                          v-model="birthDate"
-                          @input="setBirthDate(birthDate)">
+                          v-model="person.birthDate">
             </b-form-input>
         </b-form-group>
 
@@ -91,8 +91,7 @@ Vue.component("person-form", {
                       label="Employed:"
                       label-for="inputEmployed">
             <b-form-radio-group
-                    v-model="employed"
-                    @input="setEmployed(employed)"
+                    v-model="person.employed"
                     id="inputEmployed">
                 <b-form-radio value="1">Yes</b-form-radio>
                 <b-form-radio value="0">No</b-form-radio>
@@ -105,8 +104,7 @@ Vue.component("person-form", {
                       label-for="inputSalaty">
             <b-form-input id="inputSalary"
                           placeholder="insert salary here"
-                          v-model="salary"
-                          @input="setSalary(salary)">
+                          v-model="person.salary">
             </b-form-input>
         </b-form-group>
 
@@ -116,8 +114,7 @@ Vue.component("person-form", {
                       label-for="inputNotes">
             <b-form-textarea id="inputNotes"
                       placeholder="insert notes about the candidate here"
-                      v-model="notes"
-                      @input="setNotes(notes)"
+                      v-model="person.notes"
                       :rows="3"
                       no-resize>
             </b-form-textarea>
@@ -131,24 +128,24 @@ Vue.component("person-form", {
                 <b-col cols = "3">
                     <b-form-input id="inputContactName"
                                   placeholder="name (ex: Gmail)"
-                                  v-model="contactName">
+                                  v-model="person.contactName">
                     </b-form-input>
                 </b-col>
                 <b-col cols = "8">
                     <b-form-input id="inputContactLink"
                                   placeholder="link (ex: mariorossi@gmail.com)"
-                                  v-model="contactLink">
+                                  v-model="person.contactLink">
                     </b-form-input>
                 </b-col>
                 <b-col cols = "1">
                     <b-button id = "ContactsButton"
-                              variant="outline-primary"
-                              @click="setContact()">+</b-button>
+                              variant = "outline-primary"
+                              @click = "setContact()">+</b-button>
                 </b-col>
             </b-row>
 
             <b-table striped
-                     :items = "contacts"
+                     :items = "person.contacts"
                      :fields = "fields">
 
                 <template slot="action" slot-scope="row">
